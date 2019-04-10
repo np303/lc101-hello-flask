5c9d9095e35ead64ef46401ee487e04c73372d53
Nick Paisley <2ep303@gmail.com>
2019-04-10 09:48:22 -0500

    jinja 2 tutorial 1

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import Flask, request, redirect
+import cgi
+import os
+import jinja2
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader (template_dir), autoescape=True)
+ 
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
 @app.route("/")
 def index():
-    return "Hello World"
+    template = jinja_env.get_template('hello_form.html')
+    return template.render()
+
+@app.route("/hello", methods=['POST'])    
+def hello():
+    first_name = request.form['first_name']
+    template = jinja_env.get_template('hello_greeting.html')
+    return template.render(name=first_name)
     
 app.run()    
